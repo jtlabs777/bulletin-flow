c58e5c2cff242ee237df762dfc3bea0cbb1f55d7
Jacin <jtlabs777@gamil.com>
2025-11-27 19:06:04 -0500

    Fix Edit Template Fields button 404 error
    Changed route from /edit-template to /define-template since the define-template page already supports editing existing templates

--- app/dashboard/bulletins/[id]/page.tsx
+++ app/dashboard/bulletins/[id]/page.tsx
@@ -148,7 +148,7 @@ export default async function ViewBulletinPage({ params }: PageProps) {
                         </Link>
                     )}
                     {bulletin.is_template && (
-                        <Link href={`/dashboard/bulletins/${id}/edit-template`}>
+                        <Link href={`/dashboard/bulletins/${id}/define-template`}>
                             <Button variant="outline">
                                 <Edit className="mr-2 h-4 w-4" />
                                 Edit Template Fields
